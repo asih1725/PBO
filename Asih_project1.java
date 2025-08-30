9fd207e7d7cbf327cd691132c0cb78d07671da5f
setiasih <setyaasih34@gmail.com>
2025-08-30 11:11:09 +0700

    Add files via upload

---
+++ Asih_project1.java
@@ -0,0 +1,14 @@
+package com.mycompany.asih_project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Asih_project1 {
+    public static void main(String[] args) {
+        System.out.println("Nama   : Setiasih");
+        System.out.println("NPM    : 2410506011");
+        System.out.println("Alamat : Ampel, Boyolali, Jawa Tengah");
+        System.out.println("No HP  : 08588887777");
+    }
+}
