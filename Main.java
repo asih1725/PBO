b136b05d62657b473995a6114e6135a5680ed142
setiasih <setyaasih34@gmail.com>
2025-09-08 19:04:26 +0700

    praktikum3_tugas

--- Main.java
+++ Main.java
@@ -2,20 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package praktikum3;
+package praktikum3_tugas;
 
 /**
  *
  * @author ASUS
  */
- public class Main {
-     public static void main(String[] args) {
-         Hewan kucing = new Hewan("Jojo", 5);
-         kucing.suara();
-         kucing.info();
-     
-         Hewan anjing = new Hewan("Popi", 5);
-         anjing.suara();
-         anjing.info(); 
-     }
- }
+
+public class Main {
+    public static void main(String[] args) {
+        // Membuat dua object Mobil
+        Mobil mobil1 = new Mobil("Toyota", "Avanza", 2020, "Hitam");
+        Mobil mobil2 = new Mobil("Honda", "Civic", 2022, "Putih");
+
+        // Menampilkan informasi mobil
+        mobil1.displayInfo();
+        mobil2.displayInfo();
+
+        // Menyalakan mesin
+        mobil1.startEngine();
+        mobil2.startEngine();
+
+        // Mengubah warna mobil1
+        mobil1.setWarna("Merah");
+        System.out.println("Setelah diubah warna:");
+        mobil1.displayInfo();
+    }
+}
+
---
+++ Mobil.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum3_tugas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mobil {
+    private String merk;
+    private String model;
+    private int tahun;
+    private String warna; // atribut tambahan
+
+    // Constructor
+    public Mobil(String merk, String model, int tahun, String warna) {
+        this.merk = merk;
+        this.model = model;
+        this.tahun = tahun;
+        this.warna = warna;
+    }
+
+    // Getter dan Setter
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getTahun() {
+        return tahun;
+    }
+
+    public void setTahun(int tahun) {
+        this.tahun = tahun;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    // Method displayInfo
+    public void displayInfo() {
+        System.out.println("Merk   : " + merk);
+        System.out.println("Model  : " + model);
+        System.out.println("Tahun  : " + tahun);
+        System.out.println("Warna  : " + warna);
+        System.out.println("------------------------");
+    }
+
+    // Method startEngine
+    public void startEngine() {
+        System.out.println("Mesin mobil " + merk + " menyala");
+    }
+}
+
