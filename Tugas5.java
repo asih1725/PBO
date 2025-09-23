b56606776e7529e99a576ec589c140973bf0f491
setiasih <setyaasih34@gmail.com>
2025-09-24 00:46:33 +0700

    Add files via upload

---
+++ Tugas5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas5;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugas5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
